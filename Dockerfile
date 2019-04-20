79bae34d2879b92ecd6a2d106b5d688427e420b5
thelifeinro <d_pirvan@yahoo.com>
2019-04-20 13:04:26 +0300

    Changed app

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
-FROM alpine:edge
+# Using official python runtime base image
+FROM python:2.7-alpine
 
-RUN apk add --update py2-pip
+# Set the application directory
+WORKDIR /app
 
-COPY requirements.txt /usr/src/app/
+# Install our requirements.txt
+ADD requirements.txt /app/requirements.txt
+RUN pip install -r requirements.txt
 
-RUN pip install --no-cache-dir -r /usr/src/app/requirements.txt
+# Copy our code from the current folder to /app inside the container
+ADD . /app
 
-COPY app.py /usr/src/app/
+# Make port 80 available for links and/or publish
+EXPOSE 80
 
-COPY templates/index.html /usr/src/app/templates/
-
-EXPOSE 5000
-CMD ["python", "/usr/src/app/app.py"]
+# Define our command to be run when launching the container
+CMD ["gunicorn", "app:app", "-b", "0.0.0.0:80", "--log-file", "-", "--access-logfile", "-", "--workers", "4", "--keep-alive", "0"]
--- app.py
+++ app.py
@@ -1,20 +1,44 @@
-from flask import Flask, render_template
-import random
+from flask import Flask, render_template, request, make_response, g
+from redis import Redis
+import os
 import socket
-app = Flask(__name__)
-images = [
-	"https://i2.wp.com/www.libraryforsmartinvestors.com/wp-content/uploads/2017/02/aws_logo.jpg?fit=500%2C500&ssl=1",
-	"http://australiaday.openstack.org.au/wp-content/uploads/2016/01/openstack_500x500.png",
- 	"http://isoc.ae/image/cache/catalog/courses/Google%20Cloud%20Compute%20Engine%20Essential%20Training-500x500.jpg",
-	"https://static.onthehub.com/production/attachments/9/9cb7d193-f0b7-e611-9423-b8ca3a5db7a1/7d04d71c-c4e0-4df8-8e07-ff33bf12915e.png",
-	"https://aptira.com/wp-content/uploads/2016/09/kubernetes_logo.png",
-	"https://www.opsview.com/sites/default/files/docker.png"
-]
+import random
+import json
 
-@app.route('/')
-def index():
-	url = random.choice(images)
- 	return render_template('index.html', url=url, hostname=socket.gethostname())
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
+hostname = socket.gethostname()
+
+app = Flask(__name__)
+
+def get_redis():
+    if not hasattr(g, 'redis'):
+        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+    return g.redis
+
+@app.route("/", methods=['POST','GET'])
+def hello():
+    voter_id = request.cookies.get('voter_id')
+    if not voter_id:
+        voter_id = hex(random.getrandbits(64))[2:-1]
+
+    nume = None
+
+    if request.method == 'POST':
+        redis = get_redis()
+        nume = request.form['nume']
+		grupa = request.form['grupa']
+        data = json.dumps({'voter_id': voter_id, 'nume': nume, 'grupa':grupa})
+        redis.rpush('entries', data)
+
+    resp = make_response(render_template(
+        'index.html',
+        hostname=hostname,
+        nume=nume,
+    ))
+    resp.set_cookie('voter_id', voter_id)
+    return resp
+
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
